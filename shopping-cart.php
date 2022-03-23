a1ea573202e7c96736423e37d40cdf838a1e9f03
Bustarde <bustarde.audriel@auf.edu.ph>
2022-03-23 23:39:32 +0800

    Customized the design,added my own logo,updated the layout of products,and shopping cart

--- shopping-cart.php
+++ shopping-cart.php
@@ -15,15 +15,39 @@ $shoppingCartItems = $shoppingCart->getAllItems();
 ?>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>My Cart</title>
 </head>
 <body>
 
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
-<h2>Shopping Cart</h2>
-<h4>
-    <a href="products-list.php">Shop More Products</a>
-</h4>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">
+            <img src="logo.png" height="35">
+        </a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="#" class="nav-item nav-link active">Home</a>
+                <a href="#" class="nav-item nav-link active">About</a>
+                <a href="#" class="nav-item nav-link active">Contact</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<div style="background: url(header2.png)" class="jumbotron bg-cover text-white">
+    <div class="container py-5 text-center">
+        <h1 class="display-4 font-weight-bold" style="color: white">Welcome <?php echo $customer->getName() ?>!</h1><br>
+        <h2 class="font-italic mb-0" style="color: orange"><b>SHOPPING CART</b></h2><br><br>
+        <a href="products-list.php">Shop More Products</a>
+    </div>
+</div>
 
 <?php if (count($shoppingCartItems) > 0): ?>
 
